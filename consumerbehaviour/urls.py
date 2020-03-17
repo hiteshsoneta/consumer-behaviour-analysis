a56c35e7848f64d2a9c5c5f53b6e279e2890070d
Hitesh <2016.hitesh.soneta@ves.ac.in>
2020-03-17 16:59:18 +0530

    profile page updated

--- consumerbehaviour/urls.py
+++ consumerbehaviour/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('polls.urls'), name='index'),
@@ -30,3 +32,8 @@ urlpatterns = [
 
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
